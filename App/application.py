1eb6c52cbcbaae88218dc830139d4d6b6d4c6018
ANDRES ESTRADA <andrebestrada@gmail.com>
2021-08-26 20:05:17 -0500

    Change Files Structure to Deploy app in AWS

--- App/application.py
+++ App/application.py
@@ -1,61 +1,16 @@
 from flask import Flask, jsonify
-#from flask_pymongo import PyMongo
 from flask import request
 from flask import Response
+from flask import render_template
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 import numpy as np
-import dateutil
 import pandas as pd
-import os
-import json
-
-# OH_X_train=[][]
-# OH_X_valid=[][]
-# y_train=[][]
-# y_valid=[][]
 
 application = Flask(__name__)
-
-#mongo_db = "Spotifydb"
-#mongo = PyMongo(app, uri=f'mongodb://localhost:27017/{mongo_db}')
-#collection = mongo.db.Top200byCountry
-
-# This function receives some parameters that will be used to group by and summarize the data 
-def group_data(data,column,value,headers):
-    data_df = pd.DataFrame(data)
-
-    grouped = data_df.groupby(column).sum().sort_values(by=[value],ascending=False).reset_index()
-    grouped = grouped[headers]
-
-    response = Response(grouped.to_json(orient="records"), mimetype='application/json')
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
-
-# Function that retrieves parameters from end point and uses them to create an array of objects that will be used to filter data from Mongo
-def filter_data():
-    filters={}
-    date_filter={}
-    args = request.args
-    for k, v in args.items():
-        
-        if(v != "" and k != 'Group_by'): 
-            if(k == 'datefrom' or k == 'dateto'): 
-                if(k == 'datefrom'):
-                    date_filter['$gte'] = dateutil.parser.parse(v)
-                    filters["Date"] = date_filter
-                if(k == 'dateto'):
-                    date_filter['$lt'] = dateutil.parser.parse(v)
-                    filters["Date"] = date_filter
-            else:
-                filters[k] = v
-    
-    return filters
-
 
 # Function that receives data from AWS Bucket
 def data_loading():
@@ -63,9 +18,11 @@ def data_loading():
     national_data = pd.read_json('https://national-data.s3.amazonaws.com/national_data_complete.json')
     national_data = national_data.fillna(0)
 
-    args = request.args
-    print(args)
+    national_data = national_data[national_data['currency']=='MXN']
+    national_data = national_data[national_data['name'].str.contains('Renta')==False]
+    national_data = national_data[national_data['cp'].isnull()==False]
 
+    args = request.args
 
     for k, v in args.items():
         if k == "State" and v != '':
@@ -87,20 +44,6 @@ def data_loading():
         if k == "Max_enviroments" and v != '':
             national_data = national_data[national_data['Ambientes'] < float(v)]
                     
-
-    # print(args.get('Min_price'))
-    # validate_null()
-
-    # national_data = national_data[national_data['Estado']==state]
-    # national_data = national_data[national_data['price']>min_price]
-    # national_data = national_data[national_data['price']<max_price]
-
-    national_data = national_data[national_data['currency']=='MXN']
-    national_data = national_data[national_data['name'].str.contains('Renta')==False]
-    national_data = national_data[national_data['cp'].isnull()==False]
-
-    # national_data = national_data[national_data['price']<10000000]
-    # national_data = national_data[national_data['Superficie total']<300]
 
     return national_data
 
@@ -132,7 +75,7 @@ def split_and_encoding(data):
 
     return OH_X_train, OH_X_valid, y_train, y_valid
 
-
+# Function that receives a data set and a model, fits the model and return performance measures
 def machine_learning_model(data, model):
     OH_X_train, OH_X_valid, y_train, y_valid = split_and_encoding(data)
 
@@ -175,6 +118,10 @@ def machine_learning_model(data, model):
     print(results)
     return results, model
 
+@application.route("/")
+def index():
+    return render_template('index.html')
+
 
 @application.route("/dropdowns")
 def dropdowns():
@@ -199,7 +146,6 @@ def results():
     #################################################################################
     # Print Parameters
     args = request.args
-    print(args)
 
     #################################################################################
     # Loading Data
@@ -240,8 +186,6 @@ def boxes():
     
 @application.route("/prediction")
 def prediction():
-    
-    input_prediction = pd.DataFrame()
     #################################################################################
     # Loading Data
     national_data = data_loading()
@@ -254,21 +198,35 @@ def prediction():
     # Machine Learning Function
     results, model = machine_learning_model(national_data, model)
     
-    cp_default = national_data['cp'].mode()
-    surfaceTot_default = national_data['Superficie total'].mode()
-    room_default = national_data['Recamaras'].mode()
-    bathroom_default = national_data['Banos'].mode()
-    surface_Cons_default = national_data['Superficie construida'].mode()
-    parkinglot_default = national_data['Estacionamientos'].mode()
+    import statistics
+    
+    cp_default = statistics.median(national_data['cp'])
+    surfaceTot_default = statistics.median(national_data['Superficie total'])
+    room_default = statistics.median(national_data['Recamaras'])
+    bathroom_default = statistics.median(national_data['Banos'])
+    surface_Cons_default = statistics.median(national_data['Superficie construida'])
+    parkinglot_default = statistics.median(national_data['Estacionamientos'])
+
+    antiguedad_default = statistics.median(national_data['Antiguedad'])
+    enviroment_default = statistics.median(national_data['Ambientes'])
+    mantenimiento_default = statistics.median(national_data['Cuota mensual de mantenimiento'])
+    bodegas_default = statistics.median(national_data['Bodegas'])
+    floor_default = statistics.median(national_data['Cantidad de pisos'])
+    
+
+    # cp_default = national_data['cp'].mode()
+    # surfaceTot_default = national_data['Superficie total'].mode()
+    # room_default = national_data['Recamaras'].mode()
+    # bathroom_default = national_data['Banos'].mode()
+    # surface_Cons_default = national_data['Superficie construida'].mode()
+    # parkinglot_default = national_data['Estacionamientos'].mode()
 
     # Default inputs
-    antiguedad_default = national_data['Antiguedad'].mode()
-    enviroment_default = national_data['Ambientes'].mode()   
-    mantenimiento_default = national_data['Cuota mensual de mantenimiento'].mode()
-    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-    print(national_data['Bodegas'])
-    bodegas_default = national_data['Bodegas'].mode()
-    floor_default = national_data['Cantidad de pisos'].mode()
+    # antiguedad_default = national_data['Antiguedad'].mode()
+    # enviroment_default = national_data['Ambientes'].mode()   
+    # mantenimiento_default = national_data['Cuota mensual de mantenimiento'].mode()
+    # bodegas_default = national_data['Bodegas'].mode()
+    # floor_default = national_data['Cantidad de pisos'].mode()
 
     house_default = 0
     apartment_default = 1
@@ -289,12 +247,12 @@ def prediction():
         if k == "parking_lots" and v != '':
             parkinglot_default = v
     
+    input_prediction = pd.DataFrame()
     input_prediction = pd.DataFrame({'cp':cp_default, 'Superficie construida':surface_Cons_default,'Recamaras': room_default, 
                                  'Estacionamientos': parkinglot_default, 'Cantidad de pisos':floor_default,
                                  'Bodegas':bodegas_default, 'Superficie total':surfaceTot_default, 'Ambientes':enviroment_default,
                                  'Banos':bathroom_default, 'Antiguedad':antiguedad_default, 'Cuota mensual de mantenimiento':mantenimiento_default,
-                                '0':house_default,'1':apartment_default})
-    print("################################################################################")
+                                '0':house_default,'1':apartment_default},index=[0]) 
     print(input_prediction)
 
     prediction_result = model.predict(input_prediction)
@@ -304,80 +262,6 @@ def prediction():
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
-
-
-# @app.route("/songs")
-# def jsonified():
-
-#     group = request.args.get("Group_by")
-
-#     songs = [i for i in collection.find(filter_data())]    
-#     for song in songs: song.pop("_id")
-
-#     # This is commented only for testing
-#     # response = jsonify(songs)
-#     # response.headers.add('Access-Control-Allow-Origin', '*')
-#     # return response
-#     response = group_data(songs, group,"Streams",[group,"Streams"]) 
-
-#     return response
-
-
-# @app.route("/streamsbydate")
-# def jsonified2():
-#     group = request.args.get("Group_by")
-
-#     songs = [i for i in collection.find(filter_data())]    
-#     for song in songs: song.pop("_id")
-
-#     data_df = pd.DataFrame(songs)
-#     grouped = data_df.groupby(group).sum().reset_index()
-#     grouped["MonthYear"] = (grouped['MonthYear'].dt.strftime('%b')).astype(str)+" "+(pd.DatetimeIndex(grouped['MonthYear']).year).astype(str)
-    
-#     response = Response(grouped.to_json(orient="records"), mimetype='application/json')
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-
-#     return response
-
-# @app.route("/summary")
-# def jsonified3():
-#     songs = [i for i in collection.find(filter_data())]    
-#     for song in songs: song.pop("_id")
-
-#     data_df = pd.DataFrame(songs)
-    
-#     country_array=[]
-#     artist_array=[]
-#     song_array=[]
-
-#     country_array = (data_df["Country"].unique())
-#     country_array = np.insert(country_array,0,"")
-
-#     artist_array = np.insert(((data_df["Artist"].unique())[0:500]),0,"")
-#     song_array = np.insert(((data_df["Track_Name"].unique())[0:500]),0,"")
-
-#     no_artist = len(data_df["Artist"].unique())
-#     no_songs = len(data_df["Track_URL"].unique())
-#     total_streams = data_df["Streams"].sum()
-
-#     summary= pd.DataFrame.from_dict({'ArtistCount':[no_artist],'SongsCount':[no_songs],'TotalStreams':[total_streams],'Countries':[country_array],'Artists':[artist_array],'Songs':[song_array]})
-
-#     response = Response(summary.to_json(orient="records"), mimetype='application/json')
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-
-#     return response
-
-# @app.route("/Popularity")
-# def jsonified4():
-    
-#     songs = [i for i in mongo.db.TopGlobal.find()]    
-#     for song in songs: song.pop("_id")
-
-#     # This is commented only for testing
-#     response = jsonify(songs)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
 
 
 if __name__ == "__main__":
